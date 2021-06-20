cd4425be65a5a82a05d49867a8a905945f6cbfc6
Will <will@dev.webisabi.co.uk>
2021-06-20 01:14:02 +0100

    local-dev update

--- app/Artwork.php
+++ app/Artwork.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Traits\Spatial;
 class Artwork extends Model
 {
-    //
+    // add location coordinates
+    use Spatial;
+    protected $spatial = ['location'];
 }
